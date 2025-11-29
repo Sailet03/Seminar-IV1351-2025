f64cadd921b74c9198192c402d88dc50b1587548
Elias Tosteberg <elias.tosteberg@gmail.com>
2025-11-29 17:55:48 +0100

    Q3  Q2,Q1 fix

--- Seminar2/Query.sql
+++ Seminar2/Query.sql
@@ -8,13 +8,13 @@ c.HP AS "HP",
 i.Period AS "Period", 
 i.RegisterdStudents AS "# Students", 
 
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture') AS "Lecture Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial') AS "Tutorial Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab') AS "Lab Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar') AS "Seminar Hours",
-SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other')  AS "Other Overhead Hours",
-SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours')  AS "Admin",
-SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours')  AS "Exam",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) AS "Lecture Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) AS "Tutorial Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) AS "Lab Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) AS "Seminar Hours",
+COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0)  AS "Other Overhead Hours",
+COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0)  AS "Admin",
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)  AS "Exam",
 
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) +
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) +
@@ -22,7 +22,7 @@ COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) +
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) +
 COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0) +
 COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0) +
-COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'))
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)
 AS "Total Hours"
 
 FROM CourseLayout c
@@ -50,13 +50,13 @@ c.HP AS "HP",
 p.Name AS "Teacher's Name",
 t.TitleDesignation AS "Designation",
 
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture') AS "Lecture Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial') AS "Tutorial Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab') AS "Lab Hours",
-SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar') AS "Seminar Hours",
-SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other')  AS "Other Overhead Hours",
-SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours')  AS "Admin",
-SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours')  AS "Exam",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) AS "Lecture Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) AS "Tutorial Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) AS "Lab Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) AS "Seminar Hours",
+COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0)  AS "Other Overhead Hours",
+COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0)  AS "Admin",
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)  AS "Exam",
 
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) +
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) +
@@ -64,7 +64,7 @@ COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) +
 COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) +
 COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0) +
 COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0) +
-COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'))
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)
 AS "Total Hours"
 
 FROM CourseLayout c
@@ -88,6 +88,54 @@ i.RegisterdStudents,
 i.Period,
 p.Name,
 t.TitleDesignation
+ORDER BY
+p.Name,
+i.StudyYear;
+
+--3. Calculate total alocated hours for a teacher
+
+
+SELECT c.Code AS "Course Code", 
+(CAST(i.StudyYear AS TEXT) ||'-'|| i.InstanceID) AS "Course Instance ID", 
+c.HP AS "HP", 
+p.Name AS "Teacher's Name",
+
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) AS "Lecture Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) AS "Tutorial Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) AS "Lab Hours",
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) AS "Seminar Hours",
+COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0)  AS "Other Overhead Hours",
+COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0)  AS "Admin",
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)  AS "Exam",
+
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lecture'),0) +
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Tutorial'),0) +
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Lab'),0) +
+COALESCE(SUM(ROUND(pa.Hours * a.Factor)) FILTER (WHERE pa.Type = 'Seminar'),0) +
+COALESCE(SUM(ROUND(pa.Hours)) FILTER (WHERE pa.Type = 'Other'),0) +
+COALESCE(SUM(ROUND(2 * c.HP + 28 + a.HpFactor)) FILTER (WHERE pa.Type = 'AdminHours'),0) +
+COALESCE(SUM(ROUND(32 + a.StudentFactor * i.RegisterdStudents)) FILTER (WHERE pa.Type = 'ExamHours'),0)
+AS "Total Hours"
+
+FROM CourseLayout c
+    JOIN Instance  i ON c.LayoutID = i.LayoutID
+    JOIN PlannedActivities pa ON pa.CourseInstanceID = i.InstanceID
+    JOIN ActivityType a ON pa.Type = a.Name
+    JOIN EmployeesPlannedActivities epa ON epa.ActivityID = pa.ActivityID
+    JOIN Employees e ON e.EmpoyeID = epa.EmpoyeID
+    JOIN Person p ON e.PersonID = p.PersonID
+
+
+WHERE p.Name = 'Frank Miller' --Decides what teacher to filter for
+
+GROUP BY
+c.Code,
+i.StudyYear,
+i.InstanceID,
+c.HP,
+i.RegisterdStudents,
+i.Period,
+p.Name
 ORDER BY
 p.Name,
 i.StudyYear;
